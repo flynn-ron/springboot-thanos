d6609c41a9aead4897d0f4263870ed9948eeadb7
ronghl <huilin_rong@163.com>
2023-05-16 10:37:22 +0800

    feat(boot-kafka-mq) kafka消费示例

---
+++ boot-kafka-mq/src/main/java/boot/kafka/KafkaConsumerExample.java
@@ -0,0 +1,37 @@
+package boot.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        String bootstrapServers = "localhost:9092";
+        String groupId = "my-group";
+        String topic = "topic_test";
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singleton(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("Received message: key=%s value=%s%n", record.key(), record.value());
+            }
+        }
+    }
+
+}
